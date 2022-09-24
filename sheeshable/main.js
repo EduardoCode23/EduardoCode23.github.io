17e698e2c1bdd137c92b2a47e091bc5d5323ea8a
EduardoCode23 <106855879+EduardoCode23@users.noreply.github.com>
2022-09-24 12:32:27 +0800

    Fixed position updated

--- sheeshable/main.js
+++ sheeshable/main.js
@@ -7,6 +7,32 @@ function dropdownMenu() {
     }
 }
 
+function friend() {
+
+    var sectionleft = document.getElementById("section-left");
+    if (sectionleft.className === "section-left") {
+        sectionleft.className += "Responsive";
+    } else {
+        sectionleft.className = "section-left"
+    }
+
+    var men = document.getElementById("section-right");
+    if (men.className === "section-right") {
+        men.className += "Responsive";
+    } else {
+        men.className = "section-right";
+    }
+
+    var main = document.getElementById("container");
+    if (main.className === "container") {
+        men.className += "Responsive";
+    } else {
+        men.className = "container";
+    }
+
+
+}
+
 function btnclick() {
     let click = "Di pa nagana yan parr";
     alert(click);
